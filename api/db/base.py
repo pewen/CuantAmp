f09413ac2b0bc64ee1812f3da2f28c666b9b15fc
fnbellomo <fnbellomo@gmail.com>
2021-07-02 10:38:26 -0300

    The base crud also update the metrics table

--- api/db/base.py
+++ api/db/base.py
@@ -13,6 +13,7 @@ from pydantic import BaseModel as PydanticBaseModel
 from sqlalchemy import Column, DateTime, Integer
 from sqlalchemy.sql import func
 from sqlalchemy.orm import Session
+from sqlalchemy.inspection import inspect
 from sqlalchemy.ext.declarative import declarative_base
 from fastapi.encoders import jsonable_encoder
 
@@ -43,6 +44,8 @@ def to_camel(string: str) -> str:
 
 
 class BaseSchema(PydanticBaseModel):
+    id: int
+
     class Config:
         orm_mode = True
         alias_generator = to_camel
@@ -50,20 +53,31 @@ class BaseSchema(PydanticBaseModel):
 
 
 ModelType = TypeVar("ModelType", bound=BaseModel)
+MetricsModelType = TypeVar("MetricsModelType", bound=BaseModel)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=PydanticBaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=PydanticBaseModel)
 
 
-class BaseCRUD(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
-    def __init__(self, model: Type[ModelType]):
+def fill_model(data: dict, model: BaseModel):
+    columns = [c.name for c in inspect(model).columns]
+    data = {key: value for key, value in data.items() if key in columns}
+    return model(**data)
+
+class BaseCRUD(Generic[ModelType, MetricsModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(
+        self, model: Type[ModelType], metrics_model: Type[MetricsModelType]
+    ):
         """
         CRUD object with default methods to
-        Create, Read, Update, Delete (CRUD).
+        Create, Read, Update, Delete (CRUD)
+        object with its associated metrics.
         **Parameters**
         * `model`: A SQLAlchemy model class
+        * `metrics_model`: A SQLAlchemy model class
         * `schema`: A Pydantic model (schema) class
         """
         self.model = model
+        self.metrics_model = metrics_model
 
     def get(self, db: Session, id: Any) -> Optional[ModelType]:
         return db.query(self.model).filter(self.model.id == id).one_or_none()
@@ -75,13 +89,24 @@ class BaseCRUD(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
             .all()
         )
 
+    def get_metrics(self, db: Session, *, by: dict, order=["time desc"]) -> List[MetricsModelType]:
+        return (
+            db.query(self.metrics_model)
+            .filter_by(**by)
+            .order_by(*get_ordering_for_model(self.metrics_model, order))
+            .all()
+        )
+
     def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
         if isinstance(obj_in, dict):
             create_data = obj_in
         else:
             create_data = obj_in.dict(exclude_unset=True)
-        db_obj = self.model(**create_data)  # type: ignore
+        db_obj = fill_model(create_data, self.model)
         db.add(db_obj)
+        db_metrics_obj = fill_model(create_data, self.metrics_model)
+        db.add(db_metrics_obj)
+
         db.commit()
         db.refresh(db_obj)
         return db_obj
@@ -102,6 +127,12 @@ class BaseCRUD(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
             if field in update_data:
                 setattr(db_obj, field, update_data[field])
         db.add(db_obj)
+
+        if "id" in update_data:
+            del update_data["id"]
+        db_metrics_obj = fill_model(update_data, self.metrics_model)
+        db.add(db_metrics_obj)
+
         db.commit()
         db.refresh(db_obj)
         return db_obj
