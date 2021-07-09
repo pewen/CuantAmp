07058c1319a3630fbd8d4695df289c25e0fb8b66
fnbellomo <fnbellomo@gmail.com>
2021-07-09 16:22:10 -0300

    Wait db is up

--- dev-setup.sh
+++ dev-setup.sh
@@ -5,4 +5,5 @@ set -a
 source .env
 
 docker-compose build
-docker-compose run --rm api alembic upgrade head
+docker-compose run --rm api ./wait-for-it.sh db:5432 -- alembic upgrade head
+docker-compose run --rm api python db/seed.py
